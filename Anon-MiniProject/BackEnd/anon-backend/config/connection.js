14d80e75c326ea264487cdaa0568ed236a73698c
Atharvap7804 <atharvapawar53@gmail.com>
2025-05-04 20:13:28 +0530

    Project

--- Anon-MiniProject/BackEnd/anon-backend/config/connection.js
+++
@@ -1,9 +0,0 @@
-// server/db/connection.js
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/anon', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error: ', err));
